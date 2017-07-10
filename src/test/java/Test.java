80186c7c1833f6048db305e21b128d4a95ffdeca
s.sidoryuk@safe.com.ru <93h66nvA>
2017-07-10 15:44:32 +0300

    Test CI

---
+++ src/test/java/Test.java
@@ -0,0 +1,13 @@
+/**
+ * Created by user on 10.07.17.
+ */
+
+import org.junit.Assert;
+
+public class Test {
+
+    @org.junit.Test
+    public void test(){
+        Assert.assertTrue(true);
+    }
+}
