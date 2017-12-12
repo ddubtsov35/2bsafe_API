89f942e26c878fc73b4afe5e877fb95de0501e2e
s.sidoryuk@safe.com.ru <93h66nvA>
2017-12-12 10:59:48 +0300

    v1.0.2
    With AuthorisationChildren negative test

--- src/main/java/com/dubtsov/_2bsafe/Parents/Functions/Registration/RegistrationUserStep2Class.java
+++ src/main/java/com/dubtsov/_2bsafe/Parents/Functions/Registration/RegistrationUserStep2Class.java
@@ -26,6 +26,7 @@ public class RegistrationUserStep2Class extends BaseClass {
             response = responseClass.getJsonResponse();
 
             if(response.code() == 200) {
+                System.out.println("xxxxxxxxx " + response.body().string());
                 UserPool.setUserFromFile(GenerateRegistrationContent.getRegistrationStep1Content);
                 //GenerateProfileIdContent.profileId = null;
             }
